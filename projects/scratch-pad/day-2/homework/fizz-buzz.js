2f028985e9ce844d1aae16cc7eeea24a40b5b60b
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-07 21:33:38 +0000

    updated FizzBuzz

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -14,15 +14,19 @@ function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
 //Print every number within the range of 1 to 100; use a for loop
-    for(var i = 1; i = 100; i++){
-        if(i % 2 === 0){}
-    }
-//Make a conditional statement that says if a multiple of 3, print "Fizz" instead of number; use remainder operator 
-
+    for(var i = 1; i < 101; i++){
+   //Make a conditional statement that says if a multiple of 3, print "Fizz" instead of number; use remainder operator 
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
 //Conditional also says if a multiple of 5, print "Buzz", & for any numbers that multiples of both, print "FizzBuzz"
-
-
-
+        }else if(i % 5 === 0){
+            console.log("Buzz");
+        }else if(i % 3 === 0){
+            console.log("Fizz");
+        }else{
+            console.log(i);
+        }
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
