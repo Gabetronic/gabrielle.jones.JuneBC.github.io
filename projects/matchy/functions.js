1557f2b1f77a097a879273355287c01bbaba23dd
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-15 00:58:28 +0000

    updated matchy

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,33 +13,73 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function search(array, string){
-//Iterate through array & return the indexed value that matches the string input
-for(var i = 0; i < array.length; i++){
-    if(array[i] === string){
+//Iterate through array,
+    for(var i = 0; i < array.length; i++){
+//If the indexed value matches the string input,
+        if(string.toUpperCase() === array[i].name.toUpperCase()){
+//Return indexed value
         return array[i];
-    }else{
-        return null;
+        }
     }
-}
-
+//If no matches, return null
+    return null;
 }
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+function replace(array, string, object){
+//Iterate through array,
+    for(var i = 0; i < array.length; i++){
+//If the indexed value matches the string input,
+        if(string.toUpperCase() === array[i].name.toUpperCase()){
+//Splice to replace array value with new array value 
+        return array.splice(array[i], 1, object);
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(array, string){
+//Iterate through array,
+    for(var i = 0; i < array.length; i++){
+//If the indexed value matches the string input,
+        if(string.toUpperCase() === array[i].name.toUpperCase()){
+//Splice to remove the array value  
+            return array.splice(array[i], 1);
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function add(array, object){
+
+//If object has a name prop (length must be > 0), &
+  //If object has a species prop (length must be > 0), &
+    if(object.name.length > 0 && object.species.length > 0){
+//Iterate through array
+        for(var i = 0; i < array.length; i++){
+//If object's name != any other object.name in the animals array,
+            if(object.name !== array[i].name){
+//Add object parameter to array parameter.
+                array.push(object);
+//If conditions aren't met, return original array
+            }else{
+                return array;
+            }
+        }
+    }
+
+
+
+}
+
 
 
 
