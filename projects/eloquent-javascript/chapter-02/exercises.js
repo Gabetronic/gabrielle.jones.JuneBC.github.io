532f067723462367a3070060976c5dcfb64fa4c7
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-19 18:49:47 +0000

    updated Eloquent JS - Chap. 2

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -3,7 +3,19 @@
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function triangles() {
+function triangles(number) {
+//Write a loop that makes seven calls to console.log to output the following triangle:
+ //should log each level of triangle separately & should work for any input number
+
+//Create a string literal output
+ let string = "";
+//While the length of the string output is less than the input number parameter,
+ while(string.length < number){
+//Add a #, and reassign the string 
+   string += "#";
+//Print the reassignment  
+  console.log(string);
+ }
 
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -11,7 +23,24 @@ function triangles() {
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
-  
+//should print numbers 1 to 15 with fizz/buzz in appropriate places
+
+
+  //Print every number within the range of 1 to 100; use a for loop
+  for(var i = 1; i < 101; i++){
+    //Make a conditional statement that says for any numbers that are multiples of both 3 & 5, print "FizzBuzz" instead of the number; use remainder operator 
+         if(i % 3 === 0 && i % 5 === 0){
+             console.log("FizzBuzz");
+ //Conditional also says if a multiple of 5, print "Buzz", & 
+         }else if(i % 5 === 0){
+             console.log("Buzz");
+ //If a multiple of 3, print "Fizz" 
+         }else if(i % 3 === 0){
+             console.log("Fizz");
+         }else{
+           console.log(i);
+         }
+}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
