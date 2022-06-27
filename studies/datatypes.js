eabfb96a83077bd9492142d0f63e28834091af6c
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-27 04:52:39 +0000

    updated Studies

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -1,19 +1,43 @@
 /**
 * DATATYPES:
-*0. Intro to section
-*
-*
-*Concept explanation + a working code example + comment of what should return/print:
-* 1. Number
-* 2. String
-* 3. Boolean
-* 4. Array
-* 5. Object
-* 6. Function
-* 7. undefined
-* 8. null
-* 9. NaN
-* 10. Infinity & -Infinity
-* 11. Primitive vs. Complex types
-* 12. Copy by Value (primitive) vs. Copy by Ref
+*0. Data types are attributes of values. Javascript uses different types of dynamic(as 
+* opposed to static) data, or information, that are divided into two categories: primitive  
+* & complex.  Javascript interacts with values differently: depending on whether a 
+* value is primitive or complex, JS will direct variables to either copy by value or by 
+* reference.
 */
+
+// 1. number //
+// Numbers are read by the JS interpreter from left to right, & can be written with or without decimals.
+var numberEight = 8;
+console.log(numberEight); // prints 8
+
+// 2. string //
+// Strings hold text such as sentences, phrases, symbols & numbers.  They must be capped with either quotation 
+// marks or backticks.
+var fakeWebAddress = "www.wikipedia.com/E$M4Script";
+console.log(fakeWebAddress); // prints www.wikipedia.com/E$M4Script
+
+// 3. boolean //
+// The Boolean type will always produce a value of either true or false, depending on the data it is testing.
+var eitherOr = 10 > 100;
+console.log(eitherOr); // prints false
+
+// 4. array //
+// Arrays can hold lots of values, or "elements."  Array elements can be accessed using a measurement concept: an  
+// index number that starts at 0.  The main benefit of an array is that is can create ordered lists of elements.
+// Arrays must be wrapped in brackets.
+var myFavoriteThings = ["Raindrops on roses", "whiskers on kittens"];
+console.log(myFavoriteThings); // prints ["Raindrops on roses", "whiskers on kittens"]
+
+// 5. object //
+// Objects are alot like arrays in that they can hold a lot of values.  They are sometimes called "associative
+// arrays" because
+
+// 6. function //
+// 7. undefined //
+// 8. null //
+// 9. NaN (Not a Number) //
+// 10. Infinity & -Infinity //
+// 11. Primitive vs. Complex Types //
+// 12. Copy by Value & Copy by Reference //
