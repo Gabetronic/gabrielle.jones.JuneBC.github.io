d140a9fd8caeb3ebe1c3c2075f0c7cb38e0cec16
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-27 05:14:16 +0000

    updated studies

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -1,10 +1,9 @@
 /**
 * DATATYPES:
-*0. Data types are attributes of values. Javascript uses different types of dynamic(as 
-* opposed to static) data, or information, that are divided into two categories: primitive  
-* & complex.  Javascript interacts with values differently: depending on whether a 
-* value is primitive or complex, JS will direct variables to either copy by value or by 
-* reference.
+* 0. Data types are attributes of values. Javascript uses different types of dynamic(as 
+* opposed to static) data, or information that can be manipulated.
+* 1. Javascript interacts with values differently: depending on whether a value is primitive or complex, 
+* the Javascript engine will direct variables to either copy by value or by reference.
 */
 
 // 1. number //
@@ -31,10 +30,19 @@ var myFavoriteThings = ["Raindrops on roses", "whiskers on kittens"];
 console.log(myFavoriteThings); // prints ["Raindrops on roses", "whiskers on kittens"]
 
 // 5. object //
-// Objects are alot like arrays in that they can hold a lot of values.  They are sometimes called "associative
-// arrays" because
+// Objects are a lot like arrays in that they can hold a lot of values.  Objects are used to create relationships 
+// between values by pairing them together. Key/value pairs inside objects are seperated with a colon, and although
+// keys are strings, they are not bound by quotation marks or backticks.
+var familyTree = {
+   daughter: "Lyra Belacque, aka Lyra Silvertongue",
+   mother: "Marisa Coulter",
+   father: "Lord Asriel"
+}
+console.log(familyTree); // prints {daughter: "Lyra Belacque, aka Lyra Silvertongue", mother: "Marisa Coulter", father: "Lord Asriel"}
 
 // 6. function //
+// Functions are the action verbs of coding: they use values to produce an action, or actions.
+
 // 7. undefined //
 // 8. null //
 // 9. NaN (Not a Number) //
