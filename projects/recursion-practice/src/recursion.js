255923e73326df82a9b292099a4f4dc2728e9815
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-28 00:19:16 +0000

    updated recursion

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -4,6 +4,9 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
+
+
+////////Anything in blue in the index.html file, you don't have to do!!!!!! ///////////
 var factorial = function(n) {
 };
 
