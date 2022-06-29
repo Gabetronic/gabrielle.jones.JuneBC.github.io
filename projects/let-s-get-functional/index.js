f040fa254a49fdb1ce489369af148bd880d698bd
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-06-29 04:20:26 +0000

    updated lets get func

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -22,6 +22,11 @@ var _ = require('underbar');
  *      C) DON'T FORGET TO save your work in your regular repository!!!
  */
 
+ /**Objective**: Find the number of male customers
+ - **Input**: `Array`
+ - **Output**: `Number`
+ - **Constraints**: use `filter`
+ */
 var maleCount = function(array) {
 //Use filter method to return new array with only objects with a gendered prop of male
     var males = _.filter(array, function(customer){
@@ -31,22 +36,82 @@ return males.length; //return "a number" of male customers
 };
 
 var femaleCount;
+/*
+- **Objective**: Find the number of female customers
+- **Input**: `Array`
+- **Output**: `Number`
+- **Constraints**: use `reduce`
+*/
 
 var oldestCustomer;
+/*
+- **Objective**: Find the oldest customer's name
+- **Input**: `Array`
+- **Output**: `String`
+- **Constraints**:
+*/
 
 var youngestCustomer;
+/*
+- **Objective**: Find the youngest customer's name
+- **Input**: `Array`
+- **Output**: `String`
+- **Constraints**:
+*/
 
 var averageBalance;
+/*
+- **Objective**: Find the average balance of all customers
+- **Input**: `Array`
+- **Output**: `Number`
+- **Constraints**:
+*/
 
 var firstLetterCount;
+/*
+- **Objective**: Find how many customer's names begin with a given letter
+ - **Input**: `Array`, `Letter`
+ - **Output**: `Number`
+ - **Constraints**:
+ */
 
 var friendFirstLetterCount;
+/*
+- **Objective**: Find how many friends of a given customer have names that start with a given letter
+- **Input**: `Array`, `Customer`, `Letter`
+- **Output**: `Number`
+- **Constraints**:
+*/
 
 var friendsCount;
+/*
+- **Objective**: Find the customers' names that have a given customer's name in their friends list
+ - **Input**: `Array`, `Name`
+ - **Output**: `Array`
+ - **Constraints**:
+*/
 
 var topThreeTags;
+/*
+- **Objective**: Find the three most common tags among all customers' associated tags
+- **Input**: `Array`
+- **Output**: `Array`
+- **Constraints**:
+*/
 
 var genderCount;
+/*- **Objective**: Create a summary of genders, the output should be:
+```javascript
+{
+    male: 3,
+    female: 4,
+    non-binary: 1
+}
+```
+ - **Input**: `Array`
+ - **Output**: `Object`
+ - **Constraints**: Use `reduce`
+ */
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
