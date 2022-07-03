2e05831402a4207681fa9f2ed59ca94e4e92cf13
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-07-03 20:28:13 +0000

    updated studies

--- studies/operators.js
+++ studies/operators.js
@@ -1,7 +1,9 @@
 /**
 * OPERATORS:
-* 0. Intro to section
-*
+* 0. To save time, you can use preset instructions to perform specific math & logic-based computations.  These computations are known 
+* in Javascript, as operations; they are represented in Javascript as special symbols called operators.  Operators interact with 
+* input (values and variables), also known as operands to calculate new output(values).  There are several subgroups of operators used in 
+* Javascript: logical, comparison, unary, assignment, ternary, and arithmetic.
 *
 *Concept explanation + a working code example + comment of what should return/print:
 * 1. Assignment
@@ -11,3 +13,20 @@
 * 5. Unary(!, typeOf, -)
 * 6. Ternary
 */
+
+// 5. unary operators //
+/* 
+The logical NOT operator reverses the Boolean value of its operand. 
+*/
+var age = !true;   
+
+console.log(age); //prints => false
+
+/*
+Certain expressions are naturally considered false in Javascript: null, NaN, 0, undefined, and string literals.
+When the logical NOT operates on these values, they will always return a Boolean value of true.
+*/
+var nullValue = !null;  
+
+console.log(nullValue); //prints => true
+
--- studies/variables.js
+++ studies/variables.js
@@ -1,13 +1,38 @@
 /**
 * VARIABLES:
-* 0. Intro to section
-*
-*
-*Concept explanation + a working code example + comment of what should return/print:
-* 1. Declaration & Assignment
-* 2. var, let, const
-* 3. Hoisting
-*
-*
-*
+* 0. Variables allow you to store information in your computer program; without variables, every code statement you would make 
+* be erased after you typed it.  Variables are also called bindings, due to the nature of what they do: they grasp or hold code, 
+* so that they can be referenced, changed or copied later in the life of your program.  
+* 1. In order to get a program to hold onto a piece of code, we declare the variable.
+* 2. Variables are declared using one of three keywords: var, let, or const.  These keywords behave differently, and 
+* therefore make the variables they name behave differently.
+* 3. Once a variable is declared, you can define the variable you've created by assigning code to it. 
+* 4. To save time, you could perform both actions(declaring & assigning) by initializing a variable. 
+* 5. While Javascript interprets your code into binary, it uses an internal rule called hoisting.  Think of hoisting like an  
+* invisible pulley that always raises certain types of code to the top of your program environment.  This hoisting
+* action has important effects on the way the computer reads your code; therefore, it is good practice to keep hoisting in mind 
+* while writing code.
 */
+//2. Declaration //
+// You should declare, or name a variable. The computer will assign this declaration to the data value of undefined. 
+var myNameIs; 
+console.log(myNameIs); // prints => undefined
+
+//3. Assignment //
+//To define the variable, you should assign it to piece of code(a value or expression) or a statement.
+myNameIs = "Effia, the Beauty";
+console.log(myNameIs); // prints => Effia, the Beauty
+
+//4. Re-assignment //
+//If you change your mind, and want to redefine a variable, you need to reassign it to its new value.
+myNameIs = "Esi";
+console.log(myNameIs); // prints => Esi
+
+//5. Intializing //
+//To save time, you can name and define a variable by initializing it. 
+var myNameIs = "Esi";
+console.log(myNameIs); // prints => Esi
+
+//6. Keywords: var, let, const //
+
+//7. Hoisting //
