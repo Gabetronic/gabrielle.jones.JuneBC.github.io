e3a0157cd7c80608ea8d1b35c19e480d1220898b
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-07-04 17:45:02 +0000

    updated studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -1,6 +1,8 @@
 /**
 * CONTROL FLOW:
-* 0. Intro to control flow section
+* 0. If you wanted the Javascript interpreter to execute some code only if something is true or 
+* false, essentially, you need to control the flow of the code's execution.
+* 1. 
 *
 *
 *Concept explanation + a working code example + comment of what should return/print:
