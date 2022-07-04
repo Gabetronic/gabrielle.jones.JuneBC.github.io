40b3f9f5cb48e6d8930f69d92846aae05c5fedda
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-07-04 18:23:00 +0000

    updated studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -1,13 +1,23 @@
 /**
 * CONTROL FLOW:
-* 0. If you wanted the Javascript interpreter to execute some code only if something is true or 
+* 0. If you wanted the Javascript interpreter to execute some code only if a value is true or 
 * false, essentially, you need to control the flow of the code's execution.
-* 1. 
-*
-*
-*Concept explanation + a working code example + comment of what should return/print:
-*1. if - 
-*2. Else-if
-*3. else
-*4. switch
+* 1. If is an operator used to check if a condition is true. 
+* 2. Else-if is the next operator in the flow, or chain used to check truth, only after the previous if statement fails.
+* 3. Else is the last operator in the flow, used after all other conditional statements are proven false.
+* 4. Switch statements are a simpler way to test the value of an expression against multiple cases.
 */
+
+// 1. If statement //
+
+if(3 + 3 === 6){
+    console.log("This expression is true!");
+}
+// 2. Else-if statement //
+
+if("snake" === "iguana"){
+    console.log("These animals are the same.");
+}else if()
+// 3. Else statement //
+
+// 4. Switch statement //
