ad47b10c0cc8bb1c8cf76940b74a71803078ae07
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-07-04 01:55:43 +0000

    updated studies

--- studies/variables.js
+++ studies/variables.js
@@ -11,38 +11,38 @@
 * 5. While Javascript interprets your code into binary, it uses an internal rule called hoisting.  Think of hoisting like an  
 * invisible pulley that always raises certain types of code to the top of your program environment.  This hoisting
 * action has important effects on the way the computer reads your code; therefore, it is good practice to keep hoisting in mind 
-* while writing code.
+* while writing code by always declaring variables at the beginning of your work.
 */
-//2. Declaration //
+// 1. Declaration //
 // You should declare, or name a variable. The computer will assign this declaration to the data value of undefined. 
 var myNameIs; 
 console.log(myNameIs); // prints => undefined
 
-//3. Assignment //
-//To define the variable, you should assign it to piece of code(a value or expression) or a statement.
+// 2. Assignment //
+// To define the variable, you should assign it to piece of code(a value or expression) or a statement.
 myNameIs = "Effia, the Beauty";
 console.log(myNameIs); // prints => Effia, the Beauty
 
-//4. Re-assignment //
-//If you change your mind, and want to redefine a variable, you need to reassign it to its new value.
+// 3. Re-assignment //
+// If you change your mind, and want to redefine a variable, you need to reassign it to its new value.
 myNameIs = "Esi";
 console.log(myNameIs); // prints => Esi
 
-//5. Intializing //
-//To save time, you can name and define a variable by initializing it. 
+// 4. Intializing //
+// To save time, you can name and define a variable by initializing it. 
 var myNameIs = "Maame";
 console.log(myNameIs); // prints => Maame
 
-//6. Var keyword //
-//You can re-declare & re-assign a variable declared or initialized with the var keyword.
+// 5. Var keyword //
+// You can re-declare & re-assign a variable declared or initialized with the var keyword.
 var myAge = 35;
 myAge = 36;
 
 var myAge;
 console.log(myAge); // prints => 36
 
-//7. Let keyword //
-//You can re-assign a variable declared or initialized with the let keyword, but you can't re-declare that variable.
+// 6. Let keyword //
+// You can re-assign a variable declared or initialized with the let keyword, but you can't re-declare that variable.
 let year = 2022;
 year = 2023;
 console.log(year); // prints 2023
@@ -50,12 +50,29 @@ console.log(year); // prints 2023
 let year;
 console.log(year); // SyntaxError: Identifier 'year' has already been declared
 
-//8. Const keyword //
-//You can neither re-declare or re-assign a variable declared or initialized with the const keyword.
+// 7. Const keyword //
+// You can neither re-declare or re-assign a variable declared or initialized with the const keyword.
 const favoriteBand = "Nine Inch Nails";
-const favoriteBand; // SyntaxError: Identifier 'favoriteBand' has already been declared
+const favoriteBand; // prints SyntaxError: Identifier 'favoriteBand' has already been declared
 
 favoriteBand = "A Tribe Called Quest"; 
-console.log(favoriteBand); // TypeError: Assignment to constant variable.
+console.log(favoriteBand); // prints TypeError: Assignment to constant variable.
 
-//9. Hoisting //
+// 8. Hoisting //
+// Javascript naturally moves all variable declarations to the top of the scope they were created in - it does not move initializations.
+myCar = "Nissan";
+var myCar; 
+console.log(myCar); // prints Nissan
+
+console.log(myNewCar); // prints undefined
+var myNewCar = "Fiat";
+
+// If using let or const, Javascript will only hoist variables that have already been declared.
+
+myPhone = "iPhone 12 mini";
+let myPhone;
+console.log(myPhone); // prints ReferenceError: Cannot access 'myPhone' before initialization
+
+myOldPhone = "OnePlus 8";
+const myOldPhone;
+console.log(myOldPhone); // prints SyntaxError: Missing initializer in const declaration
