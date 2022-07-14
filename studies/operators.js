ba41749b7f2f54366846eb44919edc8f6b9a4f3d
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-07-14 15:56:51 +0000

    updated studies

--- studies/operators.js
+++ studies/operators.js
@@ -6,27 +6,20 @@
 * Javascript: logical, comparison, unary, assignment, ternary, and arithmetic.
 *
 *Concept explanation + a working code example + comment of what should return/print:
-* 1. Assignment
-* 2. Arithmetic
-* 3. Comparison
-* 4. Logical
-* 5. Unary(!, typeOf, -)
-* 6. Ternary
 */
+// 1. assignment operators //
+// 2. arithmetic operators //
+// 3. comparison operators //
+// 4. logical operators //
 
-// 5. unary operators //
-/* 
-The logical NOT operator reverses the Boolean value of its operand. 
-*/
+// The logical NOT operator reverses the Boolean value of its operand.
 var age = !true;   
-
 console.log(age); //prints => false
 
-/*
-Certain expressions are naturally considered false in Javascript: null, NaN, 0, undefined, and string literals.
-When the logical NOT operates on these values, they will always return a Boolean value of true.
-*/
+// Certain expressions are naturally considered false in Javascript: null, NaN, 0, undefined, and string literals.
+// When the logical NOT operates on these values, they will always return a Boolean value of true.
 var nullValue = !null;  
-
 console.log(nullValue); //prints => true
 
+// 5. unary operators //
+// 6. ternary operators //
