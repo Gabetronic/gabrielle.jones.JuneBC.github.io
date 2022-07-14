1b4ff1cb6f25110250d709e017b3c63113963487
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-07-14 01:39:20 +0000

    updated el.js

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -2,23 +2,40 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
-
+function flatten(array) {
+/* Use the reduce method in combination with the concat method to “flatten” an 
+array of arrays into a single array that has all the elements of the original arrays.
+let arrays = [[1, 2, 3], [4, 5], [6]]; */
+let flatArr = array.reduce(function(acc, current){
+  return acc.concat(current); // return the previousValue(accumulator)+currentValue, since this is an array, use .concat()
+}, []);
+return flatArr; //return the array, reduced
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function loop() {
+function loop(value, update, test, body) {
+/* Write a higher-order function loop that provides something like a for loop statement. 
+It takes a value, a test function, an update function, and a body function. Each iteration, 
+it first runs the test function on the current loop value and stops if that returns false. 
+Then it calls the body function, giving it the current value. Finally, it calls the update
+function to create a new value and starts from the beginning.
 
+When defining the function, you can use a regular loop to do the actual looping.*/
+  for(var value = 0; test(value); value = update(value)) {
+    body(value);
+}
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
+function every(array, test) {
+/* Implement every as a function that takes an array and a predicate function as 
+parameters. Write two versions, one using a loop and one using the some method. */
 
 }
 
@@ -26,7 +43,14 @@ function every() {
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
+function dominantDirection(text) {
+/* Write a function that computes the dominant writing direction in a string of text. 
+Remember that each script object has a direction property that can be "ltr" (left to right), 
+"rtl" (right to left), or "ttb" (top to bottom).
+
+The dominant direction is the direction of a majority of the characters that have a script 
+associated with them. The characterScript and countBy functions defined earlier in the 
+chapter are probably useful here. */
 
 }
 
